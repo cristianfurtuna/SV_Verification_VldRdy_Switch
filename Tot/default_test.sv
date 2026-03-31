7b209d60c69f020554817bbfd0b535111866e623
FlybotKiller <denisiordache15@gmail.com>
2026-03-31 10:10:12 +0300

    commit

---
+++ Tot/default_test.sv
@@ -0,0 +1,16 @@
+
+program default_test(in_interface in_intf, out_interface out_intf);
+
+    environment env;
+
+    initial begin
+        env = new(in_intf, out_intf);
+        env.gen_i.trans_ct = 4;
+        env.gen_o_0.trans_ct = 4;
+        env.gen_o_1.trans_ct = 4;
+        env.gen_o_2.trans_ct = 4;
+        env.gen_o_3.trans_ct = 4;
+        env.run();
+    end
+
+endprogram
